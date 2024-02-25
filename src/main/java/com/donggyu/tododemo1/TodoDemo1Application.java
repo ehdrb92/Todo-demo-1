9e4c5eda5ed0ff6a570a9107f5deb60f9e41d4b0
Donggyu <kcj920407@gmail.com>
2024-02-25 21:52:24 +0900

    todo project init

---
+++ src/main/java/com/donggyu/tododemo1/TodoDemo1Application.java
@@ -0,0 +1,13 @@
+package com.donggyu.tododemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoDemo1Application.class, args);
+	}
+
+}
